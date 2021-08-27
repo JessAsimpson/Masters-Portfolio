9ae9a4392e3ead2e5b34ffab11fe21a88499e2ab
Jessica Simpson <jessica@jessicas-mbp.home>
2021-08-27 17:03:33 +0100

    notes added

--- variablesOperators/functions.js
+++ variablesOperators/functions.js
@@ -1,9 +1,34 @@
 //activity 1 
-function factorial (n){
-    if((n ===0) ||(n === 1)){
+// function factorial (n){
+//     if((n ===0) ||(n === 1)){
 
-    }else {
-        return( n * factorial(n-1));
-    }
-    }
-console.log(factorial(33))
+//     }else {
+//         return( n * factorial(n-1));
+//     }
+//     }
+// console.log(factorial(33))
+
+
+const pressGrindBeans = ( => {
+    console.log("grinding for 20 seconds")
+})
+pressGrindBeans();
+// level up 
+let coffeeIsgrinding = false;
+
+const pressGrindBeans= () => {
+if (coffeeIsgrinding) {
+    console.log("stop the grind");
+    coffeeIsgrinding = false;
+} else {
+    console.log("grinding is about to begin");
+    coffeeIsgrinding = true;
+}
+}
+pressGrindBeans();
+
+
+const cashwithdrawal = (amount, accnum) => {
+    console.log('withdrawing ${amount} from account ${accnum}')
+    
+}

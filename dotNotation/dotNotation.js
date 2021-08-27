f538a72902be4bdb30e2530dcb8fe5b35884f497
Jessica Simpson <jessica@jessicas-mbp.home>
2021-08-27 14:14:56 +0100

    previous grid activity uploaded

---
+++ dotNotation/dotNotation.js
@@ -0,0 +1,30 @@
+// console.log("hello World.toUpperCase()");
+//above is a string method - method being 'to upper case' 
+//strng being 'hello world' 
+
+
+//parametres
+// console.log(Math.random()*10);
+// //or 
+
+// console.log(Math.floor(Math.random()*10));
+// //math.floor returns an integer less than or equal to the specified number
+// Math.ceil
+// //will round up to to the next largest integer
+// Math.round
+//returns the value of the number rounded to the nearest integer
+console.log("                     ");
+console.log("     |         |     ");
+console.log("     |         |     ");
+console.log("     |         |     ");
+console.log("     |         |     ");
+console.log("_ _ _ _ _ _ _ _ _ _   ")
+console.log("     |         |     ");
+console.log("     |         |     ");
+console.log("     |         |     ");
+console.log("     |         |     ");
+console.log("_ _ _ _ _ _ _ _ _ _ _")
+console.log("     |         |     ");
+console.log("     |         |     ");
+console.log("     |         |     ");
+console.log("     |         |     ");

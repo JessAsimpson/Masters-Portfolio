a0c61a6998278511d53930b67573ced56467a6e9
Jessica Simpson <jessica@jessicas-mbp.home>
2021-08-27 14:28:30 +0100

    extra notes added to this repo

--- dotNotation/dotNotation.js
+++ dotNotation/dotNotation.js
@@ -9,22 +9,33 @@
 
 // console.log(Math.floor(Math.random()*10));
 // //math.floor returns an integer less than or equal to the specified number
-// Math.ceil
-// //will round up to to the next largest integer
-// Math.round
-//returns the value of the number rounded to the nearest integer
-console.log("                     ");
-console.log("     |         |     ");
-console.log("     |         |     ");
-console.log("     |         |     ");
-console.log("     |         |     ");
-console.log("_ _ _ _ _ _ _ _ _ _   ")
-console.log("     |         |     ");
-console.log("     |         |     ");
-console.log("     |         |     ");
-console.log("     |         |     ");
-console.log("_ _ _ _ _ _ _ _ _ _ _")
-console.log("     |         |     ");
-console.log("     |         |     ");
-console.log("     |         |     ");
-console.log("     |         |     ");
+// // Math.ceil
+// // //will round up to to the next largest integer
+// // Math.round
+// //returns the value of the number rounded to the nearest integer
+
+// // grid activity 
+// console.log("                     ");
+// console.log("     |         |     ");
+// console.log("     |         |     ");
+// console.log("     |         |     ");
+// console.log("     |         |     ");
+// console.log("_ _ _ _ _ _ _ _ _ _   ")
+// console.log("     |         |     ");
+// console.log("     |         |     ");
+// console.log("     |         |     ");
+// console.log("     |         |     ");
+// console.log("_ _ _ _ _ _ _ _ _ _ _")
+// console.log("     |         |     ");
+// console.log("     |         |     ");
+// console.log("     |         |     ");
+// console.log("     |         |     ");
+// //  stretch on grid activity 
+
+// -- more reading on arrays and loops 
+
+// const = is constant and connot be changed 
+// let = is a variable you can change 
+
+//  % returns the remainder 
+// 
